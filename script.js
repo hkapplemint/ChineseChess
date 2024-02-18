f118df2b7c6b53f1c6532c30949c2e0cc534e1dc
hkapplemint <laitatwai@gmail.com>
2024-02-18 09:57:08 +0800

    new file:   img/black_advisor.png 	new file:   img/black_cannon.png 	new file:   img/black_chariot.png 	new file:   img/black_elephant.png 	new file:   img/black_general.png 	new file:   img/black_horse.png 	new file:   img/black_soldier.png 	new file:   img/chess_board.png 	new file:   img/empty_piece.png 	new file:   img/red_advisor.png 	new file:   img/red_cannon.png 	new file:   img/red_chariot.png 	new file:   img/red_elephant.png 	new file:   img/red_general.png 	new file:   img/red_horse.png 	new file:   img/red_soldier.png 	modified:   script.js

--- script.js
+++ script.js
@@ -1,5 +1,56 @@
 class ChessPiece {
-    constructor() {
-        team: red
+    constructor(team, role, originalX, originalY) {
+        this.team = team;
+        this.role = role;
+        this.positionX = originalX;
+        this.positionY = originalY;
+        this.alive = true;
+    }
+
+    move(targetX, targetY) {
+        switch(this.role) {
+            case "chariot":
+                chariotCheckMove(this.originalX, this.originalY, targetX, targetY);
+                break   
+            case "horse":
+                horseCheckMove(this.originalX, this.originalY, targetX, targetY);
+                break   
+            case "elephant":
+                elephantCheckMove(this.originalX, this.originalY, targetX, targetY);
+                break
+            case "advisor":
+                advisorCheckMove(this.originalX, this.originalY, targetX, targetY);
+                break
+            case "general":
+                generalCheckMove(this.originalX, this.originalY, targetX, targetY);
+                break
+            case "cannon":
+                cannonCheckMove(this.originalX, this.originalY, targetX, targetY);
+                break
+            case "soldier":
+                soliderCheckMove(this.originalX, this.originalY, targetX, targetY);
+                break
+            default:
+                break
+        }
+    }
+}
+
+const chariotCheckMove = (originalX, originalY, targetX, targetY) => {
+    if (originalX !== targetX && originalY === targetY) {  //moving horizontal
+
+        return
+    } else if (originalX === targetX && originalY !== targetY){  //moving vertical
+
+        return
+    }
+    return console.log("Invalid move")
+}
+
+const horseCheckMove = (originalX, originalY, targetX, targetY) => {
+    if ((originalX === targetX + 1 || originalX === targetX - 1) && originalY === targetY - 2) {  //moving up, then left or right
+        if (originalY + 1 !== ) {            //need to think
+
+        }
     }
 }
